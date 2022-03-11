b878156f066112949efd90192b7585ffdf1170e9
marquisf <marquisf@Kali>
2022-03-11 19:17:33 +0800

    Modify 53

--- 53-maximum-subarray.ts
+++ 53-maximum-subarray.ts
@@ -2,7 +2,7 @@ function maxSubArray(nums: number[]): number {
   let pre = 0; // The initial value of pre can be anything that is smaller than zero. While zero occupies the least memory space.
   let maxAnswer = nums[0];
   nums.forEach((item) => {
-      pre = Math.max(item, pre + item);
+      pre = pre < 0 ? item : (pre + item);
       maxAnswer = Math.max(maxAnswer, pre);
   });
   return maxAnswer;
