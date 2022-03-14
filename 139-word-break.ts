a572036819e486ab72d6dc156549406e0485ffda
MarquisF <zeyu@xiaohuanggua.onaliyun.com>
2022-03-15 00:07:28 +0800

    Feat: Add 139

---
+++ 139-word-break.ts
@@ -0,0 +1,36 @@
+/**
+ * The dynamic programming implementation of 139.
+ * 
+ * dp[i] ==
+ *  dp[j] &&
+ *  (If s[i ... j - 1] is included in the dictionary)
+ * 
+ * Time complexity: O(n^2)
+ * Space complexity: O(n)
+ * 
+ * @param s 
+ * @param wordDict 
+ * @returns 
+ */
+function wordBreak(s: string, wordDict: string[]): boolean {
+  const length = s.length;
+  const dp: boolean[] = [true];
+  // On leetcode, it seems the function
+  //  Array.prototype.includes
+  //  runs slower than the function Set.prototype.has.
+  //
+  // 👆 15-Mar-2022
+  //
+  const set = new Set(wordDict);
+  for (let i = 1; i <= length; i++) {
+    for (let j = 0; j < i; j++) {
+      if (dp[j] && set.has(s.slice(j, i))) {
+
+        dp[i] = true;
+        break;
+      }
+    }
+    dp[i] = !!dp[i];
+  }
+  return dp[length];
+};
