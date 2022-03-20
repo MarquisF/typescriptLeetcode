060bbe466ead1a3c7c7f03706198df4cdfae4d9a
marquisf <marquisfffff@gmail.com>
2022-03-21 06:47:54 +0800

    62 add modification

--- 62-unique-paths.ts
+++ 62-unique-paths.ts
@@ -1,5 +1,7 @@
 /**
- * C(m - 1, m - 1 + n - 1)
+ * Use math combination:
+ *  The result is C(m - 1, m - 1 + n - 1)
+ *
  *  TODO: Add detailed explaination
  *
  * @param m
@@ -7,11 +9,11 @@
  * @returns
  */
 function uniquePaths(m: number, n: number): number {
-  let ans = 1;
+  let result = 1;
 
   // x is the value that m + n going to minus.
   for (let x = 2; x <= m; x++) {
-    ans = ans * (m + n - x) / (m - (x - 1));
+    result = result * (m + n - x) / (m - (x - 1));
   }
-  return ans;
+  return result;
 };
